53bead7d131e6d05ce541c5d63e5c24bd22e1f20
ashvdk <ashwinvdrk@gmail.com>
2020-06-09 02:32:53 +0530

    added cors and mongodb connection with sample example

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose')
+app.use(cors())
+app.post("/getme",(req,res)=>{
+        const Cat = mongoose.model('Cat', { name: String });
 
-app.use((req,res)=>{
-    res.send({"message":"Hello world"})
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => res.send({"message":"saved"}));
 })
 
-app.listen(3300,()=>console.log("Listening on port 3300"));
+mongoose.connect(`mongodb+srv://ashvdk:I4OqnhVtjKg0q4RJ@cluster0-5eb4q.mongodb.net/test?retryWrites=true&w=majority`)
+.then(result => {
+    app.listen(3300,()=>console.log("Listening and connected to database"));
+})
+.catch(err => {
+    console.log(err);
+})
