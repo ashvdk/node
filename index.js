406e538d676bb0311f8299a99be2af04747a9eaf
ashvdk <ashwinvdrk@gmail.com>
2020-06-11 21:05:48 +0530

    checking 3

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const routes = require('./Route')
 //helpers
-app.use(cors())
-app.use(bodyParser.json())
-app.use(routes);
+// app.use(cors())
+// app.use(bodyParser.json())
+// app.use(routes);
 
-
+app.use((req,res)=>{
+    res.send('<p>How are you</p>');
+})
 
 //database and port
 mongoose.connect(`mongodb+srv://ashvdk:I4OqnhVtjKg0q4RJ@cluster0-5eb4q.mongodb.net/test?retryWrites=true&w=majority`)
 .then(result => {
-    app.listen(3400,()=>console.log("Listening and connected to database"));
+    app.listen(3300,()=>console.log("Listening and connected to database"));
 })
 .catch(err => {
     console.log(err);
